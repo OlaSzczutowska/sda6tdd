abd446eee04d357f7992d30b039c8645fc21a748
Aleksandra Szczutowska <ola.szczutowska93@gmail.com>
2018-02-24 15:34:11 +0100

    recipe scenario

--- src/test/java/com/sda/cucumber/CalcServiceSteps.java
+++ src/test/java/com/sda/cucumber/CalcServiceSteps.java
@@ -16,47 +16,57 @@ public class CalcServiceSteps {
     private CalcService calcService;
 
     @Given("^I initialize CalcService$")
-    public void I_initialize_CalcService(){
+    public void I_initialize_CalcService() {
         calcService = new CalcService();
     }
 
     @And("^I pass text value to compute$")
-    public void I_pass_text_value_to_compute(){
+    public void I_pass_text_value_to_compute() {
         text = "2;3;4";
     }
 
     @And("^I pass null text value$")
-    public void I_pass_null_text_value(){
+    public void I_pass_null_text_value() {
         text = null;
     }
 
     @And("^I pass blank text value$")
-    public void I_pass_blank_text_value(){
+    public void I_pass_blank_text_value() {
         text = "";
     }
 
     @And("^I pass text with letters$")
-    public void I_pass_text_with_letters(){
+    public void I_pass_text_with_letters() {
         text = "3a;2;4;5b";
     }
 
-  @When("^I execute calculate method$")
-    public void I_execute_calculate_method(){
+    @And("^I pass recipe$")
+    public void I_pass_recipe(){
+        text = "2 jablka;3 jajka;5 ziemniakow";
+    }
+
+    @When("^I execute calculate method$")
+    public void I_execute_calculate_method() {
         actual = calcService.calculate(text);
     }
 
     @Then("^I get correct result$")
-    public void I_get_correct_result(){
+    public void I_get_correct_result() {
         Assert.assertEquals(9, actual);
     }
 
     @Then("^I get 0 as a result$")
-    public void I_get_0_as_a_result(){
+    public void I_get_0_as_a_result() {
         Assert.assertEquals(0, actual);
     }
 
     @Then("^I get result for text with letters$")
-    public void  I_get_result_for_text_with_letters(){
+    public void I_get_result_for_text_with_letters() {
         Assert.assertEquals(14, actual);
+    }
+
+    @Then("^I get result for recipe$")
+    public void I_get_result_for_recipe(){
+        Assert.assertEquals(10,actual);
     }
 }
